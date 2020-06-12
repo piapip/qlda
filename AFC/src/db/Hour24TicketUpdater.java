5fd14f2940ad0fc037b309a571a66b918e4d6af3
piapip <thovinh9997@gmail.com>
2020-06-13 06:57:56 +0700

    Implement Hour24 enter updater.

--- AFC/src/db/Hour24TicketUpdater.java
+++ AFC/src/db/Hour24TicketUpdater.java
@@ -1,6 +1,9 @@
 package db;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -13,14 +16,29 @@ public class Hour24TicketUpdater implements TicketUpdater {
 
 	@Override
 	public void updateCertificateEnter(String certificateId) throws ClassNotFoundException, SQLException {
-		// TODO Auto-generated method stub
-		
+		Connection connection = ConnectToMySQL.getInformation("travelling_certificate");
+		Statement statement = connection.createStatement();
+		String sql1 = "UPDATE hour24_ticket SET status=\"" + Config.PENDING +  "\" WHERE id=\"" + certificateId+ "\"";
+		statement.executeUpdate(sql1);
+		String sql2 = "Select activeTime from hour24_ticket WHERE id='" + certificateId + "'";
+		ResultSet rs = statement.executeQuery(sql2);
+		if (rs != null) {
+			while(rs.next()){
+				String activeTime = rs.getString(1);
+				if (activeTime == null) {
+					String sql3 = "UPDATE hour24_ticket SET activeTime=\"" + getActiveTime()+   "\" WHERE id=\"" + certificateId + "\"";
+					String sql4 = "UPDATE hour24_ticket SET expiredTime=\"" + getExpiredTime()+   "\" WHERE id=\"" + certificateId + "\"";
+					statement.executeUpdate(sql3);
+					statement.executeUpdate(sql4);
+				}
+			}
+		}
+		connection.close();
 	}
 
 	@Override
 	public void updateCertificateExit(String certificateId, double fee) throws ClassNotFoundException, SQLException {
 		// TODO Auto-generated method stub
-		
 	}
 	
 	private static String getActiveTime() {
