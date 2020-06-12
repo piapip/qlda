ab11877e1ace10f6728eed814914c0830ca73758
TienNguyen <tiennguyenbka198@gmail.com>
2020-06-12 11:23:46 +0700

    saver for embarking station

--- AFC/src/db/HistorySaver.java
+++ AFC/src/db/HistorySaver.java
@@ -10,5 +10,20 @@ import java.time.format.DateTimeFormatter;
 import interactor.HistorySaverInterface;
 
 public class HistorySaver implements HistorySaverInterface{
-
+	
+	@Override
+	public void updateEmbarkingStation(String id, int stationId) throws ClassNotFoundException, SQLException {
+		// TODO Auto-generated method stub
+		Connection connection = ConnectToMySQL.getInformation("transaction_history");
+		Statement statement = connection.createStatement();
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
+		LocalDateTime now = LocalDateTime.now();
+		String sql = "UPDATE transactions SET status=" + Config.PENDING +
+				", embarking_station_ID=" + stationId +
+				", time_In=\"" + dtf.format(now) + "\"" +
+				" WHERE certificateID=\"" + id + "\" AND status=" + Config.UNUSED;
+		statement.executeUpdate(sql);
+		connection.close();
+	}
+	
 }
