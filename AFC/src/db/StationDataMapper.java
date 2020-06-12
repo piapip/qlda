46ea52c6fe6bd2145b7191c7e464e6a0eb68c5b3
piapip <thovinh9997@gmail.com>
2020-06-12 09:13:05 +0700

    Hoan thien StationDataMapper

--- AFC/src/db/StationDataMapper.java
+++ AFC/src/db/StationDataMapper.java
@@ -1,6 +1,9 @@
 package db;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import station.Station;
 import station.StationDBGateway;
@@ -9,8 +12,19 @@ public class StationDataMapper implements StationDBGateway {
 
 	@Override
 	public Station getStationById(int id) throws ClassNotFoundException, SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		Connection connection = ConnectToMySQL.getInformation("station_system");
+		Statement statement = connection.createStatement();
+		String sql = "Select * from station WHERE id='" + id + "'";
+		ResultSet rs = statement.executeQuery(sql);
+		Station result = null;
+		while(rs.next()) {
+			int stationId = rs.getInt(1);
+			String stationName = rs.getString(2);
+			double stationDistance = rs.getDouble(3);
+			result = new Station(stationId, stationName, stationDistance);
+		}
+		connection.close();
+		return result;
 	}
 	
 }
