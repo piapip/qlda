c123f1eb0832d19fb9b1d89f16c625821dfeb915
TienNguyen <tiennguyenbka198@gmail.com>
2020-06-12 23:45:41 +0700

    init class OnewayTicketDataMapper

---
+++ AFC/src/db/OnewayTicketDataMapper.java
@@ -0,0 +1,13 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import certificate.OnewayTicket;
+import certificate.TicketDBGateway;
+
+public class OnewayTicketDataMapper implements TicketDBGateway {
+
+}
