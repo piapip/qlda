7de99faadcb0eb42efb8c5f97d23b2672520f999
TienNguyen <tiennguyenbka198@gmail.com>
2020-06-12 11:16:12 +0700

    Init class history saver from implements

---
+++ AFC/src/db/HistorySaver.java
@@ -0,0 +1,14 @@
+package db;
+
+import java.sql.Connection;
+//import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import interactor.HistorySaverInterface;
+
+public class HistorySaver implements HistorySaverInterface{
+
+}
