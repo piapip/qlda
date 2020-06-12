20591452151e354c32fa364b81645f87f7b05906
TienNguyen <tiennguyenbka198@gmail.com>
2020-06-12 23:46:46 +0700

    oneway ticket getCertificateById

--- AFC/src/db/OnewayTicketDataMapper.java
+++ AFC/src/db/OnewayTicketDataMapper.java
@@ -9,5 +9,30 @@ import certificate.OnewayTicket;
 import certificate.TicketDBGateway;
 
 public class OnewayTicketDataMapper implements TicketDBGateway {
-
+	 
+	@Override
+	public OnewayTicket getCertificateById(String id) throws SQLException, ClassNotFoundException {
+		Connection connection = ConnectToMySQL.getInformation("travelling_certificate");
+		Statement statement = connection.createStatement();
+		String sql = "Select * from certificate_info WHERE id='" + id +"'";
+		ResultSet rs = statement.executeQuery(sql);
+		OnewayTicket result = null;
+		if (rs != null) {
+			while(rs.next()) {
+				int type = rs.getInt(2);
+				if (type != Config.ONEWAY_TYPE) {
+					System.out.println("This is not an oneway ticket!");
+					return null;
+				}
+				result = new OnewayTicket(id,
+						getStatus(id), 
+						getStartingStation(id), 
+						getEndingStation(id), 
+						getFee(id));
+			}
+		}
+		connection.close();
+		return result;
+	}
+	
 }
