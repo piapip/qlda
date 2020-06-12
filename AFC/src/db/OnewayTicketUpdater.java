26209886683973e5959ff78f59a95f3ed0cb7323
piapip <thovinh9997@gmail.com>
2020-06-12 09:15:58 +0700

    Hoan thien OnewayTicketUpdater

--- AFC/src/db/OnewayTicketUpdater.java
+++ AFC/src/db/OnewayTicketUpdater.java
@@ -1,6 +1,8 @@
 package db;
 
+import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import interactor.TicketUpdater;
 
@@ -9,13 +11,21 @@ public class OnewayTicketUpdater implements TicketUpdater {
 	@Override
 	public void updateCertificateEnter(String certificateId) throws ClassNotFoundException, SQLException {
 		// TODO Auto-generated method stub
-		
+		Connection connection = ConnectToMySQL.getInformation("travelling_certificate");
+		Statement statement = connection.createStatement();
+		String sql = "UPDATE oneway_ticket SET status=" + Config.PENDING + " WHERE id=\"" + certificateId + "\"";
+		statement.executeUpdate(sql);
+		connection.close();
 	}
 
 	@Override
 	public void updateCertificateExit(String certificateId, double fee) throws ClassNotFoundException, SQLException {
 		// TODO Auto-generated method stub
-		
+		Connection connection = ConnectToMySQL.getInformation("travelling_certificate");
+		Statement statement = connection.createStatement();
+		String sql = "UPDATE oneway_ticket SET status=" + Config.EXPIRED + " WHERE id=\"" + certificateId + "\"";
+		statement.executeUpdate(sql);
+		connection.close();
 	}
 
 }
