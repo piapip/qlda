48777ac9ecd410c0c41d7e2fec033ab1c2acb29f
piapip <thovinh9997@gmail.com>
2020-06-12 09:12:05 +0700

    Khoi tao StationDataMapper

---
+++ AFC/src/db/StationDataMapper.java
@@ -0,0 +1,16 @@
+package db;
+
+import java.sql.SQLException;
+
+import station.Station;
+import station.StationDBGateway;
+
+public class StationDataMapper implements StationDBGateway {
+
+	@Override
+	public Station getStationById(int id) throws ClassNotFoundException, SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
