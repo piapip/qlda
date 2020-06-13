da5c8ab5793ddb02fa5fa34c153d3df86293d06e
NGUYEN DICH LONG <longnguyen25111998@gmail.com>
2020-06-14 00:00:25 +0700

    add: implement passExiting method of RequirementHour24Ticket class

--- AFC/src/requirements/RequirementHour24Ticket.java
+++ AFC/src/requirements/RequirementHour24Ticket.java
@@ -27,14 +27,23 @@ public class RequirementHour24Ticket implements RequirementInterface{
 		if(historyGW.getLastHistoryByCertificateId(certificateId).getStatus() != Config.UNUSED || 
 				historyGW.getLastHistoryByCertificateId(certificateId).getStatus() != Config.SUCCESSFUL) {
 			return "You can't enter the station with this ticket.";
-		
+		}
 		return null;
 	}
 
 	@Override
 	public String passExiting(String certificateId, double fee) throws ClassNotFoundException, SQLException {
+		Hour24Ticket ticket = (Hour24Ticket) ticketGW.getCertificateById(certificateId);
+		if (ticket == null) return "Ticket doesn't exist. Please buy a new one.";
+		if(ticket.getStatus() == Config.EXPIRED) {
+			return "The ticket is already used.";
+		}
+		if (historyGW.getLastHistoryByCertificateId(certificateId).getStatus() != Config.PENDING) {
+			return "You can't exit the station with this ticket.";
+		}
+		if (ticket.isExpired()) 
+			return "The ticket is outdated! It can't be used anymore";
 		return null;
 	}
-
 }
 
