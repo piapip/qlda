4731d66cc7cdd7462a1d2bc79a9933922c2b048f
TienNguyen <tiennguyenbka198@gmail.com>
2020-06-13 09:57:06 +0700

    Init Hour24TicketDataMapper

---
+++ AFC/src/db/Hour24TicketDataMapper.java
@@ -0,0 +1,13 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import certificate.Hour24Ticket;
+import certificate.TicketDBGateway;
+
+public class Hour24TicketDataMapper implements TicketDBGateway  {
+
+}
