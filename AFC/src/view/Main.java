02b4163314eeb0bec4e53c4e4695d36ab6ecd34e
piapip <thovinh9997@gmail.com>
2020-06-15 14:18:16 +0700

    Update view.

--- AFC/src/view/Main.java
+++ AFC/src/view/Main.java
@@ -3,16 +3,29 @@ package view;
 import java.sql.SQLException;
 import java.util.Scanner;
 
+import controller.Controller;
+import db.HistoryDataMapper;
+import db.HistorySaver;
+import db.OnewayTicketDataMapper;
+import db.OnewayTicketUpdater;
+import db.PrepaidCardDataMapper;
+import db.PrepaidCardUpdater;
+import db.StationDataMapper;
 import hust.soict.se.customexception.InvalidIDException;
 import hust.soict.se.gate.Gate;
 import hust.soict.se.recognizer.TicketRecognizer;
 import hust.soict.se.scanner.CardScanner;
+import requirements.RequirementOnewayTicket;
+import requirements.RequirementPrepaidCard;
+import station.StationDistanceByDistance;
 
 public class Main {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException, InvalidIDException, InterruptedException {
 		int stationId;
 		int scanOption;
 		int enterOrExit;
+		String error;
+		Controller controller;
 		@SuppressWarnings("resource")
 		Scanner reader = new Scanner(System.in);
 		Gate gate = Gate.getInstance();
@@ -35,6 +48,21 @@ public class Main {
 				reader.nextLine();
 			} while(scanOption > 2 || scanOption < 1);
 			
+			if(scanOption == 2) 
+				controller = new Controller(
+						new RequirementOnewayTicket(new OnewayTicketDataMapper(), new HistoryDataMapper()), 
+						new OnewayTicketUpdater(), 
+						new StationDistanceByDistance(new StationDataMapper()), 
+						new HistorySaver(),
+						new HistoryDataMapper());
+			else 
+				controller = new Controller(
+						new RequirementPrepaidCard(new PrepaidCardDataMapper(), new HistoryDataMapper()), 
+						new PrepaidCardUpdater(), 
+						new StationDistanceByDistance(new StationDataMapper()), 
+						new HistorySaver(),
+						new HistoryDataMapper());
+			
 			System.out.println("Please enter your barcode: ");
 			String barCode = reader.nextLine();
 			
@@ -48,10 +76,17 @@ public class Main {
 				certificateId = ticketRecognizer.process(barCode);
 			}
 			
-			System.out.println(certificateId);
-			gate.open();
-			Thread.sleep(2000);
-			gate.close();
+			if(enterOrExit == 1) error = controller.enter(certificateId, stationId);
+			else error = controller.exit(certificateId, stationId);
+			
+			if(error == null) {
+				gate.open();
+				Thread.sleep(2000);
+				gate.close();
+			} else {
+				System.out.println("ERRRRRRR!");
+				System.out.println(error);
+			}
 			
 		} while(true);
 	}
