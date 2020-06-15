499353660febcfe560d1b2d3d90fd5a1120b3b51
piapip <thovinh9997@gmail.com>
2020-06-15 14:10:03 +0700

    Implement update prepaid card function(exit).

--- AFC/src/db/PrepaidCardUpdater.java
+++ AFC/src/db/PrepaidCardUpdater.java
@@ -21,7 +21,15 @@ public class PrepaidCardUpdater implements TicketUpdater {
 	@Override
 	public void updateCertificateExit(String certificateId, double fee) throws ClassNotFoundException, SQLException {
 		// TODO Auto-generated method stub
-		
+		Connection connection = ConnectToMySQL.getInformation("travelling_certificate");
+		Statement statement = connection.createStatement();
+		double newBalance = getBalance(certificateId) - fee;
+		String sql = "UPDATE prepaid_card SET balance=\"" + newBalance +"\" WHERE id=\"" + certificateId + "\"";
+		statement.executeUpdate(sql);
+		connection.close();
+		String statusUpdater = "UPDATE prepaid_card SET status=" + Config.UNUSED + " WHERE id=\"" + certificateId + "\"";
+		statement.executeUpdate(statusUpdater);
+		connection.close();		
 	}
 	
 	private double getBalance(String certificateId) throws ClassNotFoundException, SQLException {
