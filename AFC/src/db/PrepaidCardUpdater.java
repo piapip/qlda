f627e5807d1efb320266121478d7647ce364987b
piapip <thovinh9997@gmail.com>
2020-06-15 14:08:45 +0700

    Implement getBalance function.

--- AFC/src/db/PrepaidCardUpdater.java
+++ AFC/src/db/PrepaidCardUpdater.java
@@ -1,6 +1,9 @@
 package db;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import interactor.TicketUpdater;
 
@@ -16,6 +19,21 @@ public class PrepaidCardUpdater implements TicketUpdater {
 	public void updateCertificateExit(String certificateId, double fee) throws ClassNotFoundException, SQLException {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	private double getBalance(String certificateId) throws ClassNotFoundException, SQLException {
+		Connection connection = ConnectToMySQL.getInformation("travelling_certificate");
+		Statement statement = connection.createStatement();
+		String sql = "Select * from prepaid_card WHERE id='" + certificateId + "'";
+		ResultSet rs = statement.executeQuery(sql);
+		double result = 0;
+		if(rs != null) {
+			while(rs.next()){
+				result = rs.getDouble(2);
+			}
+		}
+		connection.close();
+		return result;
 	}
 
 }
