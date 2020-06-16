376fd01a605b64b532576c3971636de96766218e
TienNguyen <tiennguyenbka198@gmail.com>
2020-06-16 14:34:06 +0700

    Prepaid Card getcertificateById

--- sources/AFC/src/db/PrepaidCardDataMapper.java
+++ sources/AFC/src/db/PrepaidCardDataMapper.java
@@ -10,4 +10,25 @@ import certificate.TicketDBGateway;
 
 public class PrepaidCardDataMapper implements TicketDBGateway{
 	
+	@Override
+	public PrepaidCard getCertificateById(String id) throws SQLException, ClassNotFoundException {
+		Connection connection = ConnectToMySQL.getInformation("travelling_certificate");
+		Statement statement = connection.createStatement();
+		String sql = "Select * from certificate_info WHERE id='" + id +"'";
+		ResultSet rs = statement.executeQuery(sql);
+		PrepaidCard result = null;
+		if (rs != null) {
+			while(rs.next()) {
+				int type = rs.getInt(2);
+				if (type != Config.PREPAID_TYPE) {
+					System.out.println("This is not a prepaid card!");
+					return null;
+				}
+				result = new PrepaidCard(id, getBalance(id));
+			}
+		}
+		connection.close();
+		return result;
+	}
+	
 }
