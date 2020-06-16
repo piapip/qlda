4775f35b96bdf5b6fd30b674fdd358cda0a12a32
TienNguyen <tiennguyenbka198@gmail.com>
2020-06-16 14:33:22 +0700

    init PrepaidCardDataMapper

---
+++ sources/AFC/src/db/PrepaidCardDataMapper.java
@@ -0,0 +1,13 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import certificate.PrepaidCard;
+import certificate.TicketDBGateway;
+
+public class PrepaidCardDataMapper implements TicketDBGateway{
+	
+}
