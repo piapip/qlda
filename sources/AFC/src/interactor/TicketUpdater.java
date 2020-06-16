d8e1dde77fb121f0ce9f3afad3736efb77cdf7d1
NGUYEN DICH LONG <longnguyen25111998@gmail.com>
2020-06-16 08:44:21 +0700

    docs: add javadoc comment for interactor package's classes

--- sources/AFC/src/interactor/TicketUpdater.java
+++ sources/AFC/src/interactor/TicketUpdater.java
@@ -2,7 +2,27 @@ package interactor;
 
 import java.sql.SQLException;
 
+/**
+ * The Interface TicketUpdater.
+ */
 public interface TicketUpdater {
+	
+	/**
+	 * Update certificate status to "enter".
+	 *
+	 * @param certificateId the certificate id
+	 * @throws ClassNotFoundException the class not found exception
+	 * @throws SQLException the SQL exception
+	 */
 	public void updateCertificateEnter(String certificateId) throws ClassNotFoundException, SQLException;
+	
+	/**
+	 * Update certificate status to "exit".
+	 *
+	 * @param certificateId the certificate id
+	 * @param fee the fee
+	 * @throws ClassNotFoundException the class not found exception
+	 * @throws SQLException the SQL exception
+	 */
 	public void updateCertificateExit(String certificateId, double fee) throws ClassNotFoundException, SQLException;
 }
